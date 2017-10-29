da3cab28bda227ef7183b842c99eff2d35a2774e
Sairam Nutheti <snutheti@paypal.com>
2017-10-29 11:08:22 -0700

    UI code pushed

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -13,8 +13,9 @@ import axios from 'axios';
 import React, { Component } from 'react';
 import FileUpload from '../../components/HomePage/FileUpload';
 
+const surveys = [{}]
 export default function HomePage() { // eslint-disable-line react/prefer-stateless-function
   return (
-    <FileUpload surveys={surveys}/>
+    <FileUpload surveys={surveys} />
   );
 }
