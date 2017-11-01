e1b6469853e2b205b59cdfd7714b48cd9c6c72c9
Sairam Nutheti <snutheti@paypal.com>
2017-11-01 07:38:47 -0700

    resolved merge conflicts

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -33,7 +33,7 @@ export default function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Route exact path="/" component={auth.login} />
         <Route exact path="/login" component={auth.login} />
         <Route exact path="/results/:surveyName" component={ResultsPage} />
           <Route exact path="/home" component={HomePage} />
